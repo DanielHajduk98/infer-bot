fbf445d45454c2e7d606047d2c89d11d124fb4c6
kamil <kamilbartczak2019@outlook.com>
2021-10-13 16:45:41 +0200

    Introduce Flow Class &  Component Mounter func

---
+++ src/core/core.utils.js
@@ -0,0 +1,46 @@
+import { isThisTypeNode } from "typescript"
+import { createApp, h, ref } from "vue"
+
+export const initializeComponent = (component, props, children, parentSelector) => {
+    const componentInstance = createApp({
+        setup(){
+            return () => h(component, props, children)
+        }
+    }) 
+    const wrapper = document.createElement("div")
+    componentInstance.mount(wrapper)
+    document.querySelector(parentSelector).append(wrapper)
+}
+
+export class Flow{
+
+    step = 0
+
+    constructor(){
+        this.step = 0
+        this.flow = []
+    }
+    
+    resolve(){
+        this.step = this.step + 1
+        this.flow[this.step]?.handler()
+    }
+
+    addStep(handler){
+        const stepObject = {
+            id: this.flow.length,
+            resolve: this.resolve.bind(this),
+            handler
+        }
+        this.flow.push(stepObject)
+        return stepObject
+    }
+
+    next(){
+        return this.flow[this.step]
+    }
+
+    init(){
+        this.flow[0].handler()
+    }
+}
