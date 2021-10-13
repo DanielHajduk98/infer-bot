b01ea83c01c0eaea34c32fdb517a7e886a10007e
danielhajduk98 <danhajduk@protonmail.com>
2021-10-13 14:02:01 +0200

    Fixed linter

---
+++ src/pages/Chat.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <button class="btn">asdfasdf</button>
+    <button class="btn--disabled">asdfasdf</button>
+  </div>
+</template>
+
+<style lang="scss">
+.btn {
+  background: red;
+
+  &--disabled {
+    background: blue;
+  }
+}
+</style>
--- src/stores/counter/index.ts
+++ src/stores/counter/index.ts
@@ -1,15 +1,17 @@
-import { defineStore } from 'pinia'
+// Example store
 
-export const useIncrementer = defineStore('main', {
-    state: () => ({
-        number: 0
-    }),
-    getters: {
-        getNumber: state => state.number
-    },
-    actions: {
-        increment() {
-            this.number++;
-        }
-    }
-})
+// import { defineStore } from 'pinia'
+
+// export const useIncrementer = defineStore('main', {
+//     state: () => ({
+//         number: 0
+//     }),
+//     getters: {
+//         getNumber: state => state.number
+//     },
+//     actions: {
+//         increment() {
+//             this.number++;
+//         }
+//     }
+// })
