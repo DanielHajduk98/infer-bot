941efceb41bf0f3681c5d13283c1ddcce2853940
danielhajduk98 <danhajduk@protonmail.com>
2021-10-13 12:51:19 +0200

    Added linter
    Linter uses Prettier config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,22 @@
-<script setup lang="ts">
-  import {computed} from "vue";
-  import {useIncrementer} from "../stores/counter";
+<script setup>
+import { computed } from "vue";
+import { useIncrementer } from "../stores/counter";
 
-  const counter = useIncrementer()
-  const count = computed(() => counter.count)
-  console.log(counter);
-  counter.number++
-  console.log(counter.getNumber)
-  counter.$patch({ number: counter.number + 1 })
-  console.log(counter.getNumber)
-  counter.increment();
-  console.log(counter.getNumber)
+const counter = useIncrementer();
+const count = computed(() => counter.count);
+console.log(counter);
+counter.number++;
+console.log(counter.getNumber);
+counter.$patch({ number: counter.number + 1 });
+console.log(counter.getNumber);
+counter.increment();
+console.log(counter.getNumber);
 </script>
 
 <template>
-  <h1>{{ count }}</h1>
+  <h1>
+    {{ count }}
+  </h1>
 </template>
 
 <style scoped>
