5edfdd868e3f695e8dd4e0e03b55f9131c80e231
danielhajduk98 <danhajduk@protonmail.com>
2021-10-13 11:50:46 +0200

    Added standard gitignore

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import {computed} from "vue";
+  import {useIncrementer} from "../stores/counter";
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+  const counter = useIncrementer()
+  const count = computed(() => counter.count)
+  console.log(counter);
+  counter.number++
+  console.log(counter.getNumber)
+  counter.$patch({ number: counter.number + 1 })
+  console.log(counter.getNumber)
+  counter.increment();
+  console.log(counter.getNumber)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <h1>{{ count }}</h1>
 </template>
-
-<script lang="ts">
-import {useIncrementer} from "../stores/counter";
-
-export default {
-  setup() {
-    const counter = useIncrementer()
-    console.log(counter);
-    counter.number++
-    console.log(counter.getNumber)
-    // with autocompletion ✨
-    counter.$patch({ number: counter.number + 1 })
-
-    console.log(counter.getNumber)
-    // or using an action instead
-    counter.increment();
-
-    console.log(counter.getNumber)
-  },
-}
-</script>
 
 <style scoped>
 a {
