aa8879678ffc580175c140aade38acd59335a319
kamil bartczak <kamilbartczak2021@gmail.com>
2021-10-28 12:28:31 +0200

    Add results component

--- src/Results.vue
+++ src/Results.vue
@@ -1,9 +1,112 @@
 <template>
-<h1>Hello</h1>
+    <div class="recomendation">
+        <h2 class="recomendation__heading">Call an ambulance</h2>
+        <p class="recomendation__paragraph">Their symptoms are very serious, and they may require emergency care. Do not delay. Call an ambulance right now.</p>
+    </div>
+
+    <div class="alarming">
+        <h2 class="alarming__heading">Alarming Symptoms</h2>
+        <ul>
+            <li>Confusion after an injury</li>
+            <li>Shortness of breath starting within the hour</li>
+            <li>Blood oxygen level between 90 and 94%</li>
+            <li>Breathing problems after an injury</li>
+            <li>Sudden breathing problems after an injury</li>
+        </ul>
+    </div>
+
+    <div class="results">
+        <h2 class="results__heading">Results</h2>
+        <p class="results__paragraph">Please note that the list below may not be complete and is provided solely for informational purposes and is not a qualified medical opinion.</p>
+
+        <ConditionPercentage class="results__condition" name="Cancer"/>
+        <ConditionPercentage class="results__condition" name="Cancer"/>
+        <ConditionPercentage class="results__condition" name="Cancer"/>
+        <ConditionPercentage class="results__condition" name="Cancer"/>
+    </div>
 </template>
 
-<script>
+<script setup>
 </script>
 
+<style>
+#app {
+    background-color: #fff;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: min-content min-content 1fr;
+    padding-bottom: 0 !important;
+    background-color: red;
+}
+</style>
+
 <style lang="scss" scoped>
+.recomendation {
+    &__heading {
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 22px;
+        color: #112950;
+        margin-top: 0;
+    }
+
+    &__paragraph {
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 19px;
+        color: #696969;
+    }
+}
+
+.alarming {
+    &__heading {
+        margin-top: 0;
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 22px;
+        color: #112950;
+    }
+
+    ul {
+        padding: 0 0 0 13px;
+        list-style-type: disc;
+    }
+
+    ul > li {
+        font-weight: normal;
+        font-size: 12px;
+        line-height: 12px;
+        margin-bottom: 9px;
+        color: #696969;
+    }
+}
+
+.results {
+    background-color: #3F94FF;
+    color: #fff;
+    height: 100%;
+    transform: translateX(-30px);
+    width: calc(100% + 60px);
+    padding: 15px 30px;
+
+    &__heading {
+        font-weight: 600;
+        font-size: 15px;
+        line-height: 22px;
+    }
+
+    &__paragraph {
+        font-size: 10px;
+        line-height: 16px;
+        margin-bottom: 18px;
+    }
+
+    &__condition {
+        margin-bottom: 15px;
+    }
+
+    &__condition::last-child {
+        margin-bottom: 0;
+    }
+}
 </style>
---
+++ src/components/ConditionPercentage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="display">
+        <div class="display__name">{{name}}</div>
+        <div class="display__bar-container">
+            <div class="display__bar" :style="`width: ${percentage};`"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue"
+
+const props = defineProps({
+    name: {
+        type: String,
+        default: "{{default string}}"
+    },
+    percentage: {
+        type: Number,
+        default: 75
+    }
+})
+
+const percentage = computed(() => props.percentage + "%")
+</script>
+
+<style lang="scss" scoped>
+.display {
+    background-color: #fff;
+    padding: 15px;
+    border-radius: 3px;
+    &__name {
+        color: #112950;
+        font-size: 12px;
+        line-height: 17px;
+        font-weight: 600;
+    }
+
+    &__bar-container {
+        height: 9px;
+        width: 100%;
+        background-color: #8DC0FF;
+        margin-top: 9px;
+        border-radius: 18px;
+    }
+
+    &__bar {
+        height: 9px;
+        width: 10%;
+        background-color: #3F94FF;
+        margin-top: 9px;
+        border-radius: 18px;
+    }
+}
+</style>
